c2cbcf86cb6120c94699e5661d9a5a56da98dd8c
ghat0tkach <awesomevikram3@gmail.com>
2025-01-30 04:57:29 +0530

    fixes and preview-video
    Signed-off-by: ghat0tkach <awesomevikram3@gmail.com>

--- components/composer/RichTextEditor.tsx
+++ components/composer/RichTextEditor.tsx
@@ -8,6 +8,8 @@ import { toast } from "sonner"
 import { FieldSelector } from "../../components/composer/FieldSelector"
 import { Editor } from "./editor/Editor"
 import { isValidEmail } from '@/lib/utils'
+import { useTemplateStore } from '@/store/template-store';
+import Link from 'next/link';
 
 interface RichTextEditorProps {
   value: string;
@@ -15,8 +17,10 @@ interface RichTextEditorProps {
 }
 
 export default function RichTextEditor({ value, onChange }: RichTextEditorProps) {
+  const { getTemplate,setSelectedTemplate } = useTemplateStore()
   const [selectedField, setSelectedField] = useState<string | null>(null)
   const { setEmails, selectedRecipients, availableFields } = useRecipientStore()
+  const currentTemplate = getTemplate()
 
   const handleFieldSelect = (field: string) => {
     if (selectedRecipients.length === 0) {
@@ -43,6 +47,15 @@ export default function RichTextEditor({ value, onChange }: RichTextEditorProps)
     setSelectedField(null)
   }
 
+
+const clearTemplate = () => {
+  
+  onChange("")
+  setSelectedTemplate(null)
+  window.location.reload()  
+  toast.success("Template cleared")
+}
+
   return (
     <div className="border rounded-lg overflow-hidden">
       <div className="border-b p-2 bg-gray-50 flex flex-wrap gap-2 items-center">
@@ -62,6 +75,26 @@ export default function RichTextEditor({ value, onChange }: RichTextEditorProps)
           >
             Insert Mails from Field
           </Button>
+          {currentTemplate ? (
+            <Button
+              variant="outline"
+              size="sm"
+              onClick={() => clearTemplate()}
+              className="whitespace-nowrap"
+            >
+             Clear Template
+            </Button>
+          ):
+          <Link href="/templates" className="whitespace-nowrap">
+            <Button
+              variant="outline"
+              size="sm"
+              className="whitespace-nowrap"
+            >
+              Choose Template
+            </Button>
+            </Link>
+            }
         </div>
       </div>
       <Editor 
